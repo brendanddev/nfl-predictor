a62f1c94b9d64ae4598862a506b35c6f16caf464
Brendan Dileo <dileob23@gmail.com>
2025-08-24 18:10:07 -0400

    Update config and requirements

--- src/config.py
+++ src/config.py
@@ -11,8 +11,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 USER_ID = os.getenv("USER_ID")
+LEAGUE_ID = os.getenv("LEAGUE_ID")
 
 if not USER_ID:
     raise ValueError("SLEEPER_USER_ID not set in environment variables!")
+
+if not LEAGUE_ID:
+    raise ValueError("LEAGUE_ID not set in environment variables!")
 
 print(f"My Sleeper user ID is {USER_ID}")
