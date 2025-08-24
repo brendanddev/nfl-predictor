f36b7a7abb79bd6de30b7ad14fa8dfa2942719f1
Brendan Dileo <dileob23@gmail.com>
2025-08-24 17:56:08 -0400

    Create and update config

---
+++ src/config.py
@@ -0,0 +1,18 @@
+
+""" 
+config.py
+Loads configuration variables from environment variables.
+
+Brendan Dileo, August 2025
+"""
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+USER_ID = os.getenv("USER_ID")
+
+if not USER_ID:
+    raise ValueError("SLEEPER_USER_ID not set in environment variables!")
+
+print(f"My Sleeper user ID is {USER_ID}")
