a178cb91959a823ea8762445fe6e12e671b27338
Brendan Dileo <dileob23@gmail.com>
2025-08-24 20:25:28 -0400

    Update analyzer class

--- src/analyzer.py
+++ src/analyzer.py
@@ -5,8 +5,26 @@ analyzer.py
 Brendan Dileo, August 2025
 """
 
-from config import USER_ID, LEAGUE_ID
-from api import *
+from src.config import USER_ID, LEAGUE_ID
+from src.api import *
 
 
 class FantasyAnalyzer:
+    
+    def __init__(self, league_id=LEAGUE_ID):
+        self.league_id = league_id
+        self.league_data = None
+        self.rosters = None
+        self.users = None
+        self.load_league_data()
+    
+    def load_league_data(self):
+        """Load basic league data"""
+        league_resp = get_league(self.league_id)
+        rosters_resp = get_league_rosters(self.league_id)
+        users_resp = get_all_users(self.league_id)
+        
+        if league_resp and rosters_resp and users_resp:
+            self.league_data = league_resp.json()
+            self.rosters = rosters_resp.json()
+            self.users = users_resp.json()
