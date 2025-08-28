161836c97b4ece4e7afbbc7a6a9410a3389eb392
Brendan Dileo <dileob23@gmail.com>
2025-08-28 19:53:55 -0400

    Update app to just call utility methods to print info

--- app.py
+++ app.py
@@ -7,14 +7,12 @@ Brendan Dileo, August 2025
 """
 
 from src.client import SleeperClient
+from src.utils import print_rosters, print_team_names, print_league_info, print_user_info
 
 client = SleeperClient()
 client.load_players("data/players.json")
 
-user = client.get_user("brendandileo")
-client.pretty_print(user)
-
-trending = client.get_trending_players(type="add", limit=10)
-for player in trending:
-    name = client.get_player_name(player["player_id"])
-    print(f"{name} - Count: {player['count']}")
+print_user_info(client, client.user_id)
+print_league_info(client)
+print_rosters(client)
+print_team_names(client)
