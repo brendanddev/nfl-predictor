d2475986a78d64bdd888bdd8c50f7903a6a19c6a
Brendan Dileo <dileob23@gmail.com>
2025-08-28 19:25:23 -0400

    Update app, main command line entry point

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 """ 
 app.py
+Provides a command line example of using the SleeperClient.
 
 Brendan Dileo, August 2025
 """
@@ -8,8 +9,12 @@ Brendan Dileo, August 2025
 from src.client import SleeperClient
 
 client = SleeperClient()
-user = client.get_user("")
+client.load_players("data/players.json")
+
+user = client.get_user("brendandileo")
 client.pretty_print(user)
 
-trending_players = client.get_trending_players(limit=10)
-client.pretty_print(trending_players)
+trending = client.get_trending_players(type="add", limit=10)
+for player in trending:
+    name = client.get_player_name(player["player_id"])
+    print(f"{name} - Count: {player['count']}")
