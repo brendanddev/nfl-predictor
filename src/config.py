2b0800cf16f876d8a797e77a44c0eff080436eb5
Brendan Dileo <dileob23@gmail.com>
2025-08-28 18:27:46 -0400

    Update config

--- src/config.py
+++ src/config.py
@@ -1,5 +1,18 @@
 
 """ 
 config.py
+Loads configuration variables from environment variables.
 
+Brendan Dileo, August 2025
 """
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+BASE_URL = os.getenv("BASE_URL")
+USER_ID = os.getenv("USER_ID")
+LEAGUE_ID = os.getenv("LEAGUE_ID")
+
+if not BASE_URL or not USER_ID or not LEAGUE_ID:
+    raise ValueError("One or more environment variables are missing. Please check your .env file.")
