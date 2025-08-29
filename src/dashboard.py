e04c8f92f21396becb58d2415a7e3894015cf8e3
Brendan Dileo <dileob23@gmail.com>
2025-08-29 13:39:51 -0400

    Add spinner to simulate loading data

--- src/dashboard.py
+++ src/dashboard.py
@@ -11,6 +11,7 @@ Brendan Dileo, August 2025
 import streamlit as st
 from client import SleeperClient
 import st_utils as stu
+import time
 
 client = SleeperClient()
 
@@ -22,26 +23,35 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs(["User Info", "League Info", "Rosters", "
 
 with tab1:
     if username:
-        user_data = stu.get_user_cached(client, username)
+        with st.spinner("Loading user info..."):
+            user_data = stu.get_user_cached(client, username)
+            time.sleep(1)
         stu.st_user_info(user_data)
     else:
         st.info("Enter a Sleeper username in the sidebar to view user info.")
 
 with tab2:
     if league_id:
-        league_data = stu.get_league_cached(client, league_id)
+        with st.spinner("Loading league info..."):
+            league_data = stu.get_league_cached(client, league_id)
+            time.sleep(1)
         stu.st_league_info(league_data)
     else:
         st.info("Enter a League ID in the sidebar to view league info.")
 
 with tab3:
-    rosters_data = stu.get_rosters_cached(client, league_id)
+    with st.spinner("Loading rosters..."):
+        rosters_data = stu.get_rosters_cached(client, league_id)
+        time.sleep(1)
     stu.st_rosters(rosters_data, client)
 
 with tab4:
     trend_type = st.selectbox("Trend Type", options=["add", "drop", "claim"])
     limit = st.slider("Number of Players", min_value=5, max_value=50, value=10)
-    trending_data = stu.get_trending_players_cached(client, type=trend_type, limit=limit)
+    
+    with st.spinner("Loading trending players..."):
+        trending_data = stu.get_trending_players_cached(client, type=trend_type, limit=limit)
+        time.sleep(1)
     stu.st_trending_players(trending_data, client)
 
 with tab5:
